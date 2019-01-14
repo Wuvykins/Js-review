8946a28d90137e356694034db9e67f168689e8d7
Nic Alexander <wuvykins@gmail.com>
2019-01-14 02:03:16 -0500

    added the html and js

---
+++ reviewScript.js
@@ -0,0 +1,21 @@
+//console.log("Introduction to JavaScript");
+
+function FizzBizz() {
+    for (i = 0; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 != 0) {
+            console.log("Fizz");
+        }
+        else if (i % 5 == 0 && i % 3 != 0) {
+            console.log("Buzz");
+        }
+        else if (i % 5 == 0 && i % 3 == 0) {
+            console.log("FizzBuzz");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+FizzBizz();
+
+
