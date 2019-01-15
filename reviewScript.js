f9a0142e91c967be0d1c87344799ca863dc2e524
Nic Alexander <wuvykins@gmail.com>
2019-01-14 20:39:51 -0500

    added part 3-5 on the hw, consisting of two new functions

--- reviewScript.js
+++ reviewScript.js
@@ -1,14 +1,14 @@
 //console.log("Introduction to JavaScript");
 
 function FizzBizz() {
-    for (i = 0; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 != 0) {
+    for (let i = 0; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 !== 0) {
             console.log("Fizz");
         }
-        else if (i % 5 == 0 && i % 3 != 0) {
+        else if (i % 5 === 0 && i % 3 !== 0) {
             console.log("Buzz");
         }
-        else if (i % 5 == 0 && i % 3 == 0) {
+        else if (i % 5 === 0 && i % 3 === 0) {
             console.log("FizzBuzz");
         }
         else {
@@ -18,4 +18,32 @@ function FizzBizz() {
 }
 FizzBizz();
 
+function countBs(str, letter)
+{
+    let count = 0;
+    for (let i = 0; i < str.length; i++)
+    {
+        if (str.charAt(i) === letter)
+        {
+            count++;
+        }
+    }
+    return count;
 
+}
+console.log("countBs: " + countBs('BubBles', 'B'));
+
+function countChar(str, letter)
+{
+    let count = 0;
+    for (let i = 0; i < str.length; i++)
+    {
+        if (str.charAt(i) === letter)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log("countChar: " + countChar("Orange Italian Soda", 'a'));
